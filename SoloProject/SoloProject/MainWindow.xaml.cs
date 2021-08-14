67282e2bbf21605ac34f70ce5202849da01550c3
Benjamin <h8bmf@hotmail.co.uk>
2021-08-14 12:08:31 +0100

    v0.4 Login Screen and Game work.

--- SoloProject/SoloProject/MainWindow.xaml.cs
+++ SoloProject/SoloProject/MainWindow.xaml.cs
@@ -51,23 +51,30 @@ namespace SoloProject
                 if ((string)x.Tag == "obs1" || (string)x.Tag == "obs2" || (string)x.Tag == "obs3")
                 {
                     Canvas.SetLeft(x, Canvas.GetLeft(x) - 5);
+
                     if (Canvas.GetLeft(x) < -100)
                     {
                         Canvas.SetLeft(x, 800);
-                        score += 5;
+
+                        score += .5;
+                    }
+
+                    Rect pillarHitBox = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
+
+                    if (hitBox.IntersectsWith(pillarHitBox))
+                    {
+                        EndGame();
                     }
                 }
-                Rect pillarHitBox = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
-                if (hitBox.IntersectsWith(pillarHitBox))
-                {
-                    EndGame();
-                }
+
                 if ((string)x.Tag == "clouds")
                 {
                     Canvas.SetLeft(x, Canvas.GetLeft(x) - 1);
+
                     if (Canvas.GetLeft(x) < -250)
                     {
-                        Canvas.SetLeft(x, 590);
+                        Canvas.SetLeft(x, 550);
+
                         score += .5;
                     }
 
@@ -78,9 +85,10 @@ namespace SoloProject
         {
             if (e.Key == Key.Space)
             {
-                flappyBird.RenderTransform = new RotateTransform(-29, flappyBird.Width / 2, flappyBird.Height / 2);
+                flappyBird.RenderTransform = new RotateTransform(-20, flappyBird.Width / 2, flappyBird.Height / 2);
                 gravity = -8;
             }
+
             if (e.Key == Key.R && gameOver == true)
             {
                 StartGame();
@@ -90,36 +98,50 @@ namespace SoloProject
         private void Game_KeyUp(object sender, KeyEventArgs e)
         {
             flappyBird.RenderTransform = new RotateTransform(5, flappyBird.Width / 2, flappyBird.Height / 2);
+
             gravity = 8;
         }
         private void StartGame()
         {
             Game.Focus();
+
             int temp = 300;
+
             score = 0;
+
             gameOver = false;
+
             Canvas.SetTop(flappyBird, 190);
+
             foreach (var x in Game.Children.OfType<Image>())
             {
                 if ((string)x.Tag == "obs1")
+                {
                     Canvas.SetLeft(x, 500);
+                }
                 if ((string)x.Tag == "obs2")
+                {
                     Canvas.SetLeft(x, 800);
+                }
                 if ((string)x.Tag == "obs3")
+                {
                     Canvas.SetLeft(x, 1100);
+                }
+
                 if ((string)x.Tag == "clouds")
                 {
                     Canvas.SetLeft(x, 300 + temp);
                     temp = 800;
                 }
             }
+
             gameTimer.Start();
         }
         private void EndGame()
         {
             gameTimer.Stop();
             gameOver = true;
-            txtScore.Content += "Game over! Press R to restart";
+            txtScore.Content += " Game Over!!! Press R to restart.";
         }
     }
 }
